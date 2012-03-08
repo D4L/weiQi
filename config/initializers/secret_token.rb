8e97001b672b226424bf451923a601434304f113
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-03-07 22:27:27 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeiQi::Application.config.secret_token = '3896e497ab0d3e042a37204da18097d61d703ee8594c86e8b998e97b070e6d1e2fa25de8064460d63a294bacc6957e86004de48afb5df0697053f88002ea31cc'
